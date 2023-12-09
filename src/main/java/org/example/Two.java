5d67c11c5d8184eac7d6ef04fa50eb7dc53b0fa6
Tamino Hartmann <tamino.hartmann@navvis.com>
2023-12-09 16:42:27 +0100

    Day 2: add structure

--- src/main/java/org/example/Two.java
+++ src/main/java/org/example/Two.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import lombok.Getter;
 import lombok.extern.java.Log;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 @Log
 public class Two implements Day
@@ -15,5 +17,30 @@ public class Two implements Day
 	{
 		final String input = Files.readString(file);
 		log.info(input);
+
+		var result = Arrays.stream(input.split("\n"))
+			.map(Game::new)
+			.filter(Game::isValid)
+			.map(Game::getIndex)
+			.reduce(0, Integer::sum);
+
+		log.info("Sum of valid game indexes is <" + result + ">");
+
+	}
+
+	@Getter
+	private static class Game
+	{
+		int index;
+
+		private Game(final String line)
+		{
+
+		}
+
+		private boolean isValid()
+		{
+			return false;
+		}
 	}
 }
