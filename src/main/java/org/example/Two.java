3c78d72d4691ae91e41d00df66a1abbf709d1cc1
Tamino Hartmann <tamino.hartmann@navvis.com>
2023-12-09 16:55:37 +0100

    Day 2: solve first part

--- src/main/java/org/example/Two.java
+++ src/main/java/org/example/Two.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.java.Log;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,7 +21,7 @@ public class Two implements Day
 
 		var result = Arrays.stream(input.split("\n"))
 			.map(Game::new)
-			.filter(Game::isValid)
+			.filter(g -> g.isValid(12, 13, 14))
 			.map(Game::getIndex)
 			.reduce(0, Integer::sum);
 
@@ -28,19 +29,46 @@ public class Two implements Day
 
 	}
 
-	@Getter
+	@ToString
 	private static class Game
 	{
+		@Getter
 		int index;
+
+		int maxRed = 0;
+
+		int maxGreen = 0;
+
+		int maxBlue = 0;
 
 		private Game(final String line)
 		{
-
+			var split = line.split(":");
+			index = Integer.parseInt(split[0].split(" ")[1]);
+			var games = split[1].split(";");
+			for (String game : games)
+			{
+				var colors = game.split(",");
+				for (String color : colors)
+				{
+					var colorComponents = color.trim().split(" ");
+					switch (colorComponents[1])
+					{
+						case "red" ->
+							maxRed = Math.max(maxRed, Integer.parseInt(colorComponents[0]));
+						case "green" ->
+							maxGreen = Math.max(maxGreen, Integer.parseInt(colorComponents[0]));
+						case "blue" ->
+							maxBlue = Math.max(maxBlue, Integer.parseInt(colorComponents[0]));
+						default -> log.info("Unknown color! " + color);
+					}
+				}
+			}
 		}
 
-		private boolean isValid()
+		private boolean isValid(int red, int green, int blue)
 		{
-			return false;
+			return maxRed <= red && maxGreen <= green && maxBlue <= blue;
 		}
 	}
 }
